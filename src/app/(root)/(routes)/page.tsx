02034e142f5915b9749f2b21854e3690ae8a8755
ord669 <ord24@gmail.com>
2024-02-15 02:15:21 +0200

    added aouth

--- src/app/(root)/(routes)/page.tsx
+++ src/app/(root)/(routes)/page.tsx
@@ -1,18 +1,22 @@
-import { unstable_noStore as noStore } from "next/cache";
 import Link from "next/link";
+import { auth, SignInButton, SignOutButton, UserButton } from "@clerk/nextjs";
 
 import { CreatePost } from "@/app/_components/create-post";
 import { api } from "@/trpc/server";
 
 export default async function Home() {
-  noStore();
-  const hello = await api.post.hello.query({ text: "from tRPC" });
+  const latestPost = await api.post.getLatest.query();
+
+  const { userId } = auth();
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
+        <UserButton afterSignOutUrl="/" />
+        <div>{!userId && <SignInButton />}</div>
+        <div>{!!userId && <SignOutButton />}</div>
         <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
-          Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
+          <span className="text-[hsl(280,100%,70%)]">P2P</span> App
         </h1>
         <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
           <Link
@@ -38,11 +42,7 @@ export default async function Home() {
             </div>
           </Link>
         </div>
-        <div className="flex flex-col items-center gap-2">
-          <p className="text-2xl text-white">
-            {hello ? hello.greeting : "Loading tRPC query..."}
-          </p>
-        </div>
+        <div className="flex flex-col items-center gap-2"></div>
 
         <CrudShowcase />
       </div>
@@ -50,17 +50,9 @@ export default async function Home() {
   );
 }
 
-async function CrudShowcase() {
-  const latestPost = await api.post.getLatest.query();
-
+function CrudShowcase() {
   return (
     <div className="w-full max-w-xs">
-      {latestPost ? (
-        <p className="truncate">Your most recent post: {latestPost.name}</p>
-      ) : (
-        <p>You have no posts yet.</p>
-      )}
-
       <CreatePost />
     </div>
   );
