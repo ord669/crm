448d5aec0f06302a7b0e486da261e840dc7aa707
ord669 <ord24@gmail.com>
2024-02-15 02:01:20 +0200

    added clerk

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,11 @@
-import "@/styles/globals.css";
+import "~/styles/globals.css";
 
 import { Inter } from "next/font/google";
+import { cookies } from "next/headers";
 
 import { TRPCReactProvider } from "@/trpc/react";
+
+import { ClerkProvider } from "@clerk/nextjs";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -21,10 +24,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={`font-sans ${inter.variable}`}>
-        <TRPCReactProvider>{children}</TRPCReactProvider>
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={`font-sans ${inter.variable}`}>
+          <TRPCReactProvider>{children}</TRPCReactProvider>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
